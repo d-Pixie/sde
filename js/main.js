af5f94b81b91f58c5934a4f61e560bcae24c29a1
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2017-10-26 16:06:46 +0200

    Basic AST column render.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-// initialize SVG.js
-var draw = SVG('drawing')
+function draw(AST){
+  // initialize SVG.js
+  var diagram = SVG('drawing').viewbox(0, 0, 100, 100);
+  var columns = AST.actors.length;
+  var column_margin_width = (100/columns)*0.1;
+  var column_width = (100-((columns-1)*column_margin_width))/columns;
+  var total_column_width = column_width + column_margin_width;
 
-// draw pink square
-draw.rect(100, 100).move(100, 50).fill('#f06')
+  AST.actors.forEach(function(actor, index){
+    diagram.rect(column_width, 30).move(total_column_width*index, 0).fill('#f06');
+  });
+}
+
+var ast = {
+  actors: [{name:'lb', label:'LB'},{name:'proxy', label:'Proxy'},{name:'test', label:'Test'}],
+  lb: {},
+  proxy: {},
+  test: {},
+}
+
+draw( ast );
